d0aff77496df376b119bf1153d27fb6bf76b89c7
Saheb Giri <iamsahebgiri@gmail.com>
2022-09-15 20:27:31 +0530

    chore: initial setup of reshaped

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require("next-transpile-modules")(["reshaped"]);
+
+/** @type {import("next").NextConfig} */
+const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    const rule = config.module.rules.find((rule) => "oneOf" in rule);
 
-module.exports = nextConfig
+    if (rule && rule.oneOf) {
+      rule.oneOf.forEach((loader) => {
+        if (!Array.isArray(loader.use)) return;
+
+        loader.use.forEach((item) => {
+          if (!item.loader || !item.loader.includes("css-loader")) return;
+
+          // eslint-disable-next-line no-param-reassign
+          item.options = {
+            ...item.options,
+            modules: {
+              ...item.options.modules,
+              // You can customise the way css modules output classnames in your product using:
+              // getLocalIdent: () => {}
+              mode: "local",
+            },
+          };
+        });
+      });
+    }
+
+    return config;
+  },
+});
+
+module.exports = nextConfig;
